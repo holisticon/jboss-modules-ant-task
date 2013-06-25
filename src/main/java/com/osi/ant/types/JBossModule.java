b669e56cc6ec9895ccbbf21c41f25ec1a786da5f
Simon Zambrovski <simon.zambrovski@holisticon.de>
2013-06-25 19:56:06 +0200

    Refactoring for ivy

--- src/main/java/com/osi/ant/types/JBossModule.java
+++ src/main/java/com/osi/ant/types/JBossModule.java
@@ -1,64 +1,161 @@
 package com.osi.ant.types;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.types.FileSet;
 
-public class JBossModule extends FileSet {
-	
-	private String name;
-	private String slot = "main";
-	public List<JBossModuleDependency> dependencies = new ArrayList<JBossModuleDependency>();
+/**
+ * JBoss module
+ * @author Simon Zambrovski
+ * @author David Hosier
+ */
+public class JBossModule {
 
-	public JBossModule() {}
-	
+	public static final String MAIN_SLOT = "main";
+
+	private String name;
+	private String slot = MAIN_SLOT;
+	private FileSet fileSet = new FileSet();
+	public final List<JBossModuleDependency> dependencies = new ArrayList<JBossModuleDependency>();
+	public final List<FileSet> includedFilesets = new ArrayList<FileSet>();
+
+	public JBossModule() {
+	}
+
+	/**
+	 * (Ant) adds a dependency to a different module.
+	 * @param dependency module dependency.
+	 */
+	public void addConfiguredDependency(final JBossModuleDependency dependency) {
+		dependencies.add(dependency);
+	}
+
+	/**
+	 * (Ant) adds a dependency to resources specified by this fileset.
+	 * @param filesetRef fileset with resources.
+	 */
+	public void addConfiguredResourceFileSet(final FileSet filesetRef) {
+		includedFilesets.add(filesetRef);
+	}
+
+	public void generateDescriptor(final PrintStream ps) {
+
+		// add all own
+		final List<String> resources = new ArrayList<String>();
+
+		if (isEmbeddedFileSet()) {
+			resources.addAll(Arrays.asList(this.fileSet.getDirectoryScanner().getIncludedFiles()));
+		}
+		// add all referenced
+		for (FileSet fsRefs : this.includedFilesets) {
+			resources.addAll(Arrays.asList(fsRefs.getDirectoryScanner().getIncludedFiles()));
+		}
+		final PrintHelper helper = PrintHelper.module(ps, name, slot);
+		helper.printModuleResources(resources);
+		helper.printModuleDepenendencies(this.dependencies);
+		helper.printModuleFooter();
+	}
+
+	public void generateMainSlotDescriptor(PrintStream ps) {
+		PrintHelper.moduleAlias(ps, name, MAIN_SLOT, name, slot);
+	}
+
+	/**
+	 * Retrieves true, if the local attribute dir is used.
+	 * @return true if fileset embedded.
+	 */
+	private boolean isEmbeddedFileSet() {
+		return this.fileSet.getDir() != null;
+	}
+
+	/**
+	 * Returns module name property.
+	 * @return module name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns slot name or main, if no slot provided.
+	 * @return slot name.
+	 */
+	public String getSlot() {
+		return slot;
+	}
+
+	/**
+	 * Sets name.
+	 * @param name name to set.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getName() { return name; }
-	
+
+	/**
+	 * Sets slot name.
+	 * @param slot slot.
+	 */
 	public void setSlot(String slot) {
-		this.slot = slot;
-	}
-	
-	public String getSlot() { return slot; }
-	
-	public void addConfiguredDependency(JBossModuleDependency dependency) {
-		dependencies.add(dependency);
-	}
-	
-	public void generateDescriptor(PrintStream ps) {
-		ps.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		ps.println(String.format("<module xmlns=\"urn:jboss:module:1.0\" name=\"%s\">", name));
-		ps.println();
-		ps.println("\t<resources>");
-		DirectoryScanner ds = getDirectoryScanner();
-		for (String file : ds.getIncludedFiles()) {
-			ps.println(String.format("\t\t<resource-root path=\"%s\" />", file));
+		if (!"".equals(slot)) {
+			this.slot = slot;
 		}
-		ps.println("\t</resources>");
-		ps.println();
-		if (!dependencies.isEmpty()) {
-			ps.println("\t<dependencies>");
-			for (JBossModuleDependency dependency : dependencies) {
-				ps.print(String.format("\t\t<module name=\"%s\" ", dependency.getName()));
-				if (dependency.getSlot() != null) {
-					ps.print(String.format("slot=\"%s\" ", dependency.getSlot()));
-				}
-				if (dependency.getExport() != null) {
-					ps.print(String.format("export=\"%s\" ", dependency.getExport()));
-				}
-				if (dependency.getOptional() != null) {
-					ps.print(String.format("optional=\"%s\" ", dependency.getOptional()));
-				}
-				ps.println("/>");
-			}
-			ps.println("\t</dependencies>");
-		}
-		ps.println("</module>");
 	}
+
+	/**
+	 * Sets the file set directory.
+	 * @param dir shortcut for including a fileset reference with one directory.
+	 */
+	public void setDir(String dir) {
+		this.fileSet.setDir(new File(dir));
+	}
+
+	/**
+	 * Sets the file set includes.
+	 * @param includes shortcut for including a fileset reference with one directory
+	 */
+	public void setIncludes(String includes) {
+		this.fileSet.setIncludes(includes);
+	}
+
+	/**
+	 * Sets the file set includes.
+	 * @param includes shortcut for including a fileset reference with one directory
+	 */
+	public void setExcludes(String excludes) {
+		this.fileSet.setExcludes(excludes);
+	}
+
+	/**
+	 * Retrieves directory scanners for all configures filesets.
+	 * @return directory scanners.
+	 */
+	public Set<DirectoryScanner> getDirectoryScanners() {
+		final Set<DirectoryScanner> scanners = new HashSet<DirectoryScanner>();
+		if (isEmbeddedFileSet()) {
+			scanners.add(this.fileSet.getDirectoryScanner());
+		}
+
+		for (FileSet subset : this.includedFilesets) {
+			scanners.add(subset.getDirectoryScanner());
+		}
+
+		return scanners;
+	}
+
+	/**
+	 * Returns true, if the slot is a main slot.
+	 * @return true if slot is main, false otherwise.
+	 */
+	public boolean isMainSlot() {
+		return MAIN_SLOT.equals(slot);
+	}
+
 }
--- src/main/java/com/osi/ant/types/JBossModuleDependency.java
+++ src/main/java/com/osi/ant/types/JBossModuleDependency.java
@@ -1,47 +1,129 @@
 package com.osi.ant.types;
 
+import org.apache.tools.ant.BuildException;
+
+/**
+ * Represents a module dependency on another module.
+ * @author Simon Zambrovski
+ * @author David Hosier
+ */
 public class JBossModuleDependency {
-	
+
+	enum Services {
+		NONE("none"), IMPORT("import"), EXPORT("export");
+		String value;
+
+		Services(final String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		static Services byValue(String value) {
+			for (Services service : values()) {
+				if (service.getValue().equals(value)) {
+					return service;
+				}
+			}
+			return null;
+		}
+	}
+
 	private String name;
 	private String slot = null;
 	private Boolean export = null;
 	private Boolean optional = null;
+	private Services services = null;
 
-	public JBossModuleDependency() {}
-	
-	
+	public JBossModuleDependency() {
+	}
+
+	/**
+	 * Sets module name.
+	 * @param name name to set.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * Sets module slot.
+	 * @param slot slot to set.
+	 */
 	public void setSlot(String slot) {
 		this.slot = slot;
 	}
-	
+
+	/**
+	 * Sets if the module exports.
+	 * @param export 
+	 */
 	public void setExport(boolean export) {
 		this.export = export;
 	}
-	
+
+	/**
+	 * Sets if the module is optional.
+	 * @param optional
+	 */
 	public void setOptional(boolean optional) {
 		this.optional = optional;
 	}
 
+	/**
+	 * Sets services.
+	 * @param services none, import or export.
+	 */
+	public void setServices(String services) {
+		final Services value = Services.byValue(services);
+		if (value == null) {
+			throw new BuildException(
+					String.format(
+							"Wrong value for services attribute specified: %s. Allowed values are 'none', 'import' and 'export'.",
+							services));
+		}
+	}
+
+	/**
+	 * Attribute getter.
+	 * @return name.
+	 */
 	public String getName() {
 		return name;
 	}
 
-
+	/**
+	 * Attribute getter.
+	 * @return slot or null.
+	 */
 	public String getSlot() {
 		return slot;
 	}
 
-
+	/**
+	 * Attribute getter.
+	 * @return export or null.
+	 */
 	public Boolean getExport() {
 		return export;
 	}
 
-
+	/**
+	 * Attribute getter.
+	 * @return optional or null.
+	 */
 	public Boolean getOptional() {
 		return optional;
 	}
+
+	/**
+	 * Attribute getter.
+	 * @return services or null.
+	 */
+	public Services getServices() {
+		return services;
+	}
+
 }
